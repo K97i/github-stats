65e3264d022d96b25a5f70cff0a9ef1422c3891e
K97i <105120272+K97i@users.noreply.github.com>
2023-04-24 15:20:31 +0800

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -62,7 +62,7 @@ async def generate_languages(s: Stats) -> None:
     sorted_languages = sorted(
         (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
     )
-    delay_between = 75
+    delay_between = 25
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
         color = color if color is not None else "#000000"
